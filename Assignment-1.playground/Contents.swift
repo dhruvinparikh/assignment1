bff9b151edeeb54de88977580de135965698db5b
Dhruvin Parikh <n01150847@humbermail.ca>
2018-03-14 09:55:32 -0400

    generic Function name changed to swap

--- Assignment-1.playground/Contents.swift
+++ Assignment-1.playground/Contents.swift
@@ -6,32 +6,31 @@ import PlaygroundSupport
 //Exercise1
 //Write a generic function which should be able to swap the contents of any two variables. Demonstrate it by passing integers, doubles and string types of variables.
 
-func swapGenerics<T>(a: inout T, b: inout T) {
+func swap<T>(a: inout T, b: inout T) {
     let temp = a
     a = b
     b = temp
 }
 var numb1 = 100
 var numb2 = 200
-
+print("-------------------------------------------------------")
 print("Before Swapping Int values are: \(numb1) and \(numb2)")
-swapGenerics(a: &numb1, b: &numb2)
+swap(a: &numb1, b: &numb2)
 print("After Swapping Int values are: \(numb1) and \(numb2)")
-
 var str1 = "Generics"
 var str2 = "Functions"
-
+print("-------------------------------------------------------")
 print("Before Swapping String values are: \(str1) and \(str2)")
-swapGenerics(a: &str1, b: &str2)
+swap(a: &str1, b: &str2)
 print("After Swapping String values are: \(str1) and \(str2)")
-
+print("-------------------------------------------------------")
 var d1 = 4.5
 var d2 = 5.9
-
 print("Before Swapping String values are: \(d1) and \(d2)")
-swapGenerics(a: &d1, b: &d2)
+swap(a: &d1, b: &d2)
 print("After Swapping String values are: \(d1) and \(d2)")
-
+print("-------------------------------------------------------")
+print("-------------------------------------------------------")
 //Exercise2
 //Define a function – SumAvgArray(values: [Int] ) -> ( Int, Double) which calculates the sum and average of an integer array and return – sum and average ( two values ) as tuple
 
@@ -44,7 +43,8 @@ func SumAvgArray(values:[Int] ) -> (Int,Double){
 }
 var result = SumAvgArray(values:[17,18,19,20])
 print("Sum of 17, 18 ,19 and 20 is \(result.0) and average is \(result.1)")
-
+print("-------------------------------------------------------")
+print("-------------------------------------------------------")
 //Exercise3
 //Do the following using Xcode playground:
 //You are given an array of dictionaries. Each dictionary in the array contains 2 keys “firstName” and “lastName”. Create an array of strings called fullNames that contains the values for “firstName” and “lastName” from the dictionary separated by a space.
@@ -83,19 +83,23 @@ for name in arrayOfDictionaries {
     }
 }
 print(fullNames)
-
+print("-------------------------------------------------------")
+print("-------------------------------------------------------")
 //Exercise4
 //Do the following using Xcode playground:
 //You are given an array - let values = [3, 10, 6, 1, 4, 8, 2, 5, 9, 7]
 //Using Array methods filter, map, reduce and sorted, you need to multiply every odd value by 2 and then add them.
 
 let values = [3, 10, 6, 1, 4, 8, 2, 5, 9, 7]
-print("Getting odd number",values.filter {$0 % 2 != 0 })
-print("Multiplying it by 2",values.filter {$0 % 2 != 0 }.map{ $0 * 2})
-print("Sorting in ascending", values.filter {$0 % 2 != 0 }.map{ $0 * 2}.sorted())
+print("Getting odd number from \(values) is ",values.filter {$0 % 2 != 0 })
+print("-------------------------------------------------------")
+print("Multiplying 2 x \(values.filter {$0 % 2 != 0 }) is ",values.filter {$0 % 2 != 0 }.map{ $0 * 2})
+print("-------------------------------------------------------")
+print("Sorting \(values.filter {$0 % 2 != 0 }.map{ $0 * 2}) in ascending is ", values.filter {$0 % 2 != 0 }.map{ $0 * 2}.sorted())
+print("-------------------------------------------------------")
 let sum = values.filter {$0 % 2 != 0 }.map{ $0 * 2}.sorted().reduce(0, +)
-print("Sum : ",sum)
-
-
+print("Sum of \(values.filter {$0 % 2 != 0 }.map{ $0 * 2}.sorted()) is ",sum)
+print("-------------------------------------------------------")
+print("-------------------------------------------------------")
 
 
